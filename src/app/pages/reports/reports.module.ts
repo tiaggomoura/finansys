628df641c9211323cdbbefdc54c0659c0e5508c3
tiaggomoura <tiago.rodrigues.dev@gmail.com>
2020-05-10 01:01:06 -0300

    reports component

---
+++ src/app/pages/reports/reports.module.ts
@@ -0,0 +1,13 @@
+import { SharedModule } from './../../shared/shared.module';
+import { NgModule } from '@angular/core';
+import { ReportsRoutingModule } from './reports-routing.module';
+import { ReportsComponent } from './reports/reports.component';
+
+@NgModule({
+  declarations: [ReportsComponent],
+  imports: [
+    SharedModule,
+    ReportsRoutingModule
+  ]
+})
+export class ReportsModule { }
